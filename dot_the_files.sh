7020bbccef0c1faf7279691206629ccd3ef489ec
Maia <maia.mcc@gmail.com>
2017-04-08 22:02:10 -0400

    script for symlinking dotfiles

---
+++ dot_the_files.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DIR=${PWD}
+
+# TODO: make this not suck, i.e. iterate over files in directory except for
+# specific files to be ignored.
+
+# Delete mode: delete currently existing dotfiles so we can symlink new ones.
+if [[ "$1" == "-d" ]] || [[ "$1" == "-D" ]] || [[ "$1" == "--delete" ]]; then
+  echo "Deleting your current dotfiles..."
+  rm ~/.ApathyBeep.aif
+  rm ~/.DunDunDun.aiff
+  rm ~/.alias_completion.sh
+  rm ~/.bash_aliases
+  rm ~/.bash_profile
+  rm ~/.git-completion.bash
+  rm ~/.gitconfig
+  rm ~/.punlist
+  rm ~/.safety_pig
+  rm ~/.vimrc
+fi
+
+echo "Symlinking your shiny new dotfiles..."
+ln -s $DIR/ApathyBeep.aif ~/.ApathyBeep.aif
+ln -s $DIR/DunDunDun.aiff ~/.DunDunDun.aiff
+ln -s $DIR/alias_completion.sh ~/.alias_completion.sh
+ln -s $DIR/bash_aliases ~/.bash_aliases
+ln -s $DIR/bash_profile ~/.bash_profile
+ln -s $DIR/git-completion.bash ~/.git-completion.bash
+ln -s $DIR/gitconfig ~/.gitconfig
+ln -s $DIR/punlist ~/.punlist
+ln -s $DIR/safety_pig ~/.safety_pig
+ln -s $DIR/vimrc ~/.vimrc
+
+source ~/.bash_profile
+
+echo "The files have been dotted!"
